82944c366aefa31e063cafcc6a9816ac0831e833
Michael Byrd <mcb4548@uncw.edu>
2018-02-20 15:47:59 -0500

    Read in Data
    Laptop

--- SumSquare.py
+++ SumSquare.py
@@ -5,5 +5,22 @@ import time, random, math
 from Graph import *
 from MathFunctions import *
 import csv
+import ast
 from SequenceFunctions import *
+
+sequenceList = []
+
+f = open('15-39.csv', 'r')
+reader = csv.reader(f)
+for row in reader:
+    sequenceList.append((row[3]))
+f.close()
+
+
+
+# for i in range(30):
+#     sequenceList[i] = ast.literal_eval(sequenceList[i])
+#     if len(sequenceList[i]) % 2 == 0:
+#         for j in range(0, len(sequenceList[i]), 2):
+#             print(sequenceList[i][j] + sequenceList[i][j+1])
 
